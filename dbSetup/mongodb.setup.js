5c4d397401031e761fcc6f72a5bade0ab9aff080
ankitOneNineNineNine <pradhanankit12@gmail.com>
2021-06-01 16:24:52 +0545

    changes

--- dbSetup/mongodb.setup.js
+++ dbSetup/mongodb.setup.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 
 // const conxnURI = "mongodb://localhost:27017/ChatApp";
-const conxnURI =
-  "mongodb+srv://ankitProj:BARCA@nkit$980@cluster0.v1cqa.mongodb.net/ChatApp?retryWrites=true&w=majority";
 
 mongoose.connect(
   conxnURI,
