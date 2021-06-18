28e43b84115341f686ee98a8b229b1a5ec35c943
ankitOneNineNineNine <pradhanankit12@gmail.com>
2021-06-18 08:13:24 +0545

    changes

--- routes/message.route.js
+++ routes/message.route.js
@@ -22,7 +22,7 @@ router.get("/", function (req, res, next) {
     .populate("toGrp")
     .exec(function (err, messages) {
       if (err) return next(err);
-      res.status(200).json(messages);
+      res.status(200).json(messages.reverse());
     });
 });
 
