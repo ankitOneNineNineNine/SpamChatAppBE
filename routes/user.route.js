fdbe3f2fcecd7cd3ec2f2c6efa11120355f9b2cc
ankitOneNineNineNine <pradhanankit12@gmail.com>
2021-06-21 08:35:15 +0545

    changes

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,7 +3,6 @@ const UserModel = require("../model/user.model");
 const path = require("path");
 const uploadProfileImg = require("../middlewares/upload.profile");
 
-
 const streamUpload = require("../middlewares/directCloudinary");
 
 router.get("/", function (req, res, next) {
@@ -44,11 +43,7 @@ router.put(
     UserModel.findById(req.user._id).exec(async function (err, user) {
       if (err) return next(err);
       if (!user) return next({ message: "user not present" });
-      if (user.image) {
-        await require("fs/promises").unlink(
-          path.join(__dirname, `/images/profile/${user.image}`)
-        );
-      }
+
       user
         .updateOne(updatedBody)
         .then(function (data) {
